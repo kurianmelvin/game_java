844f890febd6b9b1c90ddbfd079c195e36a6e040
kurianmelvin <kurianmelvin@users.noreply.github.com>
2015-10-12 10:33:52 -0500

    Update HUD.java

--- HUD.java
+++ HUD.java
@@ -4,14 +4,11 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class HUD {
-
 	public static int HEALTH = 100;
-	
 	public void tick(){
 		//HEALTH--; // this decreases the health
 		HEALTH = Game.clamp(HEALTH, 0, 100);
 	}
-	
 	public void render(Graphics g){
 		g.setColor(Color.GRAY);
 		g.fillRect(15, 15, 200, 32);
@@ -20,5 +17,4 @@ public class HUD {
 		g.setColor(Color.WHITE);
 		g.drawRect(15, 15, 200, 32);
 	}
-	
 }
