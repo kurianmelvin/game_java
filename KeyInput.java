f1ac4b22bd4d99d3f29e4e38d4134776f71b77f9
kurianmelvin <kurianmelvin@users.noreply.github.com>
2015-10-12 10:35:49 -0500

    Update KeyInput.java

--- KeyInput.java
+++ KeyInput.java
@@ -7,15 +7,12 @@ public class KeyInput extends KeyAdapter{
 	private Handler handler;
 	public KeyInput(Handler handler){
 		 this.handler = handler;
-		
 	}
-	
 	public void keyPressed(KeyEvent e){
 		int key = e.getKeyCode();
 		System.out.println(key);
 		  for (int i = 0; i< handler.object.size(); i++){
 			GameObject tempObject = handler.object.get(i);
-			
 			if(tempObject.getId() == ID.Player){
 				// key events for player 1
 				if (key == KeyEvent.VK_W) tempObject.setVelY(-5); // this uses the letter "w" to move forward.
@@ -24,17 +21,13 @@ public class KeyInput extends KeyAdapter{
 				if (key == KeyEvent.VK_A) tempObject.setVelX(-5);
 			}
 		}
-		
 		if(key == KeyEvent.VK_ESCAPE)
 			System.exit(1);
-		  
 	}
 	public void keyReleased(KeyEvent e){
 		int key = e.getKeyCode();
-		
 		for (int i = 0; i< handler.object.size(); i++){
 			GameObject tempObject = handler.object.get(i);
-			
 			if(tempObject.getId() == ID.Player){
 				// key events for player 1
 				if (key == KeyEvent.VK_W) tempObject.setVelY(0); // this uses the letter "w" to move forward.
@@ -42,10 +35,7 @@ public class KeyInput extends KeyAdapter{
 				if (key == KeyEvent.VK_D) tempObject.setVelX(0);
 				if (key == KeyEvent.VK_A) tempObject.setVelX(0);
 			}
-						
 		}
-		
 	}
-	
 }
 
